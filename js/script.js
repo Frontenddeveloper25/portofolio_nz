82578160d6eab597f0155614d4dea783fdade286
Mac <mac@Macs-MacBook-Pro.local>
2025-05-07 23:36:45 +0700

    belajarJS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
-feather.replace();
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault();
 
-  // Toggle menu hamburger
-  const hamburger = document.getElementById('hamburger');
-  const navMenu = document.getElementById('navMenu');
+  const nama = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.(com|net|org|co\.id|id|edu)$/i;
 
-  hamburger.addEventListener('click', function () {
-    navMenu.classList.toggle('active');
-  });
+
+  if (nama === "") {
+    alert("Nama tidak boleh kosong.");
+    return;
+  }
+
+  if (nama.length < 3) {
+    alert("Pendek banget woy!!");
+    return;
+  }
+
+  if (/^\d+$/.test(nama)) {
+    alert("Nama tidak boleh hanya angka.");
+    return;
+  }
+
+  if (email === "") {
+    alert("Email tidak boleh kosong.");
+    return;
+  }
+
+  if (!emailRegex.test(email)) {
+    alert("Format email tidak valid.");
+    return;
+  }
+
+  alert(`Terima kasih, ${nama}, pesan Anda sudah dikirim!`);
+  this.reset();
+});
