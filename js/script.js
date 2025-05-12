b122816c098298ee2ed788de966a7334fddde01d
Mac <mac@Macs-MacBook-Pro.local>
2025-05-12 21:37:01 +0700

    interaksi navabr dan notif link in

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// button1
 // Menghandle interaksi saat tombol "Lihat CV" diklik
 function showInteraction(event) {
   event.preventDefault(); // Menghindari default action link (pembukaan halaman)
@@ -16,6 +18,31 @@ function showInteraction(event) {
     interactionMessage.classList.remove("show");
   }, 2000); // 2 detik delay sebelum membuka link dan menghilangkan pesan
 }
+
+function showInteraction(event) {
+  event.preventDefault(); // Menghindari default action link (pembukaan halaman)
+
+  var interactionMessage2 = document.getElementById("interactionMessage");
+  if (interactionMessage2) {
+    interactionMessage2.classList.add("show"); // Menampilkan pesan interaksi
+  }
+
+  // Setelah 2 detik, membuka link CV
+  setTimeout(function() {
+    // Membuka link CV
+    window.open("https://id.linkedin.com/in/neza-raekhan-maulana-867a92181", "_blank");
+
+    // Setelah halaman dibuka, menyembunyikan pesan interaksi
+    interactionMessage.classList.remove("show");
+  }, 2000); // 2 detik delay sebelum membuka link dan menghilangkan pesan
+}
+
+// Menghandle interaksi saat tombol "Lihat CV" diklik
+
+
+
+// ------------------------------------------------------
+
 
 // Menambahkan event listener untuk tombol "Lihat CV"
 document.getElementById("lihatCv").addEventListener("click", showInteraction);
